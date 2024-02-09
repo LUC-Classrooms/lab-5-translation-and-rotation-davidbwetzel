33da39df85a9ccc2a6add1b0e446b735dc17439e
davidbwetzel <david@wetzel-soyster.net>
2024-02-09 16:55:00 +0000

    enhanced snowman with comments and grids

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ LAB 5 - Translation and Rotation
 Move the origin point with translate() so the image appears in the center of the canvas. Then, add a conditional statement that controls the canvas rotation.
 
 */
+var a = 0;
 
 function setup() {
   createCanvas(400, 300);
@@ -12,33 +13,45 @@ function setup() {
 
 function draw() {
   background(200);
-  
+  push();
+  translate(width/2, height/2);
+  ellipse(0, 0, 100);
+  line(-width, 0, width, 0);
+  line(0, -height, 0, height);
+  pop();
+
   push(); // create a new temporary drawing layer
-  translate(width/2, height/2); // change these arguments
+  translate(width/3, height/2); // change these arguments
   
   //add an if() statement here
   if(mouseIsPressed){
-    rotate(HALF_PI);
+    a += .02;
   } else {
-    rotate(0);
+    a = 0;
   }
   
-  line(0, -50, 40, -20);
-  line(0, -50, -40, -20);
-  fill(255);
-  ellipse(0, 0, 55);
-  ellipse(0, -35, 40);
-  ellipse(0, -60, 30);
-  fill(0);
-  rectMode(CENTER);
-  rect(0, -82, 20, 20);
-  line(-20, -72, 20, -72);
-  ellipse(-7, -65, 5);
-  ellipse(7, -65, 5);
-  ellipse(0, -60, 5);
-  ellipse(0, -40, 5);
-  ellipse(0, -30, 5);
-  ellipse(0, -20, 5);
+  rotate(a);
+  stroke('blue');
+  line(-width, 0, width, 0);
+  stroke('red');
+  line(0, -height, 0, height);
+  stroke(0); // black line
+  line(0, -50, 40, -20); // right arm
+  line(0, -50, -40, -20); // left arm
+  fill(255); // white
+  ellipse(0, 0, 55); // bottom circle
+  ellipse(0, -35, 40); // middle
+  ellipse(0, -60, 30); // head
+  fill(0); // black
+  rectMode(CENTER); // center all rectangles
+  rect(0, -82, 20, 20); // top hat
+  line(-20, -72, 20, -72); // brim
+  ellipse(-7, -65, 5); // eye
+  ellipse(7, -65, 5); // eye
+  ellipse(0, -60, 5); // nose
+  ellipse(0, -40, 5); // button
+  ellipse(0, -30, 5); // button
+  ellipse(0, -20, 5); // button
 
   pop(); // dispose of the drawing layer
 }
